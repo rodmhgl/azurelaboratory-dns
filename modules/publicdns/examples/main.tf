ff4a7c17ceb36cec7a6c5e1a86f4a984993ba731
Rod Stewart <rodstewart@gmail.com>
2023-07-04 22:58:25 -0500

    Checkov should ignore this directory.

--- modules/publicdns/examples/main.tf
+++ modules/publicdns/examples/main.tf
@@ -1,18 +1,18 @@
-module "publicdomain" {
-  source              = "./modules/publicdns"
-  zone_name           = "yourdomain.com"
-  resource_group_name = "rg-publicdns"
-  tags                = {}
-  a_records = {
-    "www" = {
-      ttl     = 3600
-      records = ["12.34.56.78"]
-    }
-  }
-  cname_records = {
-    "blog" = {
-      ttl    = 300
-      record = "www.yourdomain.com"
-    }
-  }
-}
+# module "publicdomain" {
+#   source              = "./modules/publicdns"
+#   zone_name           = "yourdomain.com"
+#   resource_group_name = "rg-publicdns"
+#   tags                = {}
+#   a_records = {
+#     "www" = {
+#       ttl     = 3600
+#       records = ["12.34.56.78"]
+#     }
+#   }
+#   cname_records = {
+#     "blog" = {
+#       ttl    = 300
+#       record = "www.yourdomain.com"
+#     }
+#   }
+# }

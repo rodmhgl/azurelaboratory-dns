ab278bc703e4644ed802ed6d5bd0888e03475a5a
Rod Stewart <rodstewart@gmail.com>
2023-07-04 23:07:50 -0500

    Adds hashnode default IP

--- variables.tf
+++ variables.tf
@@ -7,4 +7,5 @@ variable "location" {
 variable "hashnodeIPAddress" {
   description = "The IP address of the Hashnode redirect."
   type        = string
+  default     = "76.76.21.21" # https://support.hashnode.com/en/articles/5755362-how-to-map-a-custom-domain
 }

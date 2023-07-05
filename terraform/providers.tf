420044105ec9bc89491f933140f81be690e8b816
Rod Stewart <rodstewart@gmail.com>
2023-07-04 22:46:34 -0500

    Adds workflow test

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,8 +7,17 @@ terraform {
       version = "3.63.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate1337"
+    container_name       = "tfstate"
+    key                  = "prod/public_dns.tfstate"
+    use_azuread_auth     = true # ARM_USE_AZUREAD
+    use_oidc             = true # ARM_USE_OIDC
+  }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
